f57f2c1086e4e3cf35a876fa61f89ae9e63e70cf
KDaria <dkdva@mail.ru>
2024-03-20 17:29:40 +0100

    Initial Commit

---
+++ Today's Cat Mood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today's Cat Mood
+//
+//  Created by Дарья Кобелева on 20.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
