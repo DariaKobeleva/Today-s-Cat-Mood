4389804e21bd42780ec9e32846edc06fb82d10f5
KDaria <dkdva@mail.ru>
2024-03-20 18:27:05 +0100

    Created ViewControllers and UIElements

---
+++ Today's Cat Mood/Services/NetworkManager.swift
@@ -0,0 +1,8 @@
+//
+//  Services.swift
+//  Today's Cat Mood
+//
+//  Created by Дарья Кобелева on 20.03.2024.
+//
+
+import Foundation
---
+++ Today's Cat Mood/ViewControllers/DetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailsViewController.swift
+//  Today's Cat Mood
+//
+//  Created by Дарья Кобелева on 20.03.2024.
+//
+
+import UIKit
+
+
+final class DetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
